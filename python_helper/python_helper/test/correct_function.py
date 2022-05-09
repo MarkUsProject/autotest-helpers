f7a1e7d5cea855c682e02b6c8cfb057ef9a3a6c9
Sophia Huynh <sophia@cs.toronto.edu>
2022-05-09 11:40:27 -0400

    Clarifying docstring in correct_function.external_buggy

--- python_helper/python_helper/test/correct_function.py
+++ python_helper/python_helper/test/correct_function.py
@@ -3,6 +3,7 @@
 
 
 def external_buggy(x: int) -> int:
-    """A buggy function that returns x.
+    """A version of buggy_function.external_buggy that correctly returns
+    x as opposed to 2 * x.
     """
     return x
