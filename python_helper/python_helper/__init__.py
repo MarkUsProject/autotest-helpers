1da40b597f206df83d4dac13860d21067cc3dda5
sophia-huynh <3385254+sophia-huynh@users.noreply.github.com>
2025-07-08 16:23:09 -0400

    python-helper: Updating bound_timeout to use wrapt_timeout_decorator (#18)
    * Replacing bound_timeout with wrapt-timeout-decorator
    
    * Adding documentation and test notes for bound_timeout changes.
    
    * Removing timeout-decorator from dependencies.

--- python_helper/python_helper/__init__.py
+++ python_helper/python_helper/__init__.py
@@ -1,4 +1,4 @@
-from .timeout import bound_timeout
+from wrapt_timeout_decorator import timeout as bound_timeout
 from .test_case_validation import get_test_cases, get_failures, get_doctest_dict
 from .code_properties import is_unimplemented, get_recursive, \
     get_functions_using, ASTParser, get_functions_that_call, is_empty
